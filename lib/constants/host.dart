dae368cb39ff78439ce54b906cf30809b8742def
maxam2017 <whk0313@gmail.com>
2021-02-12 00:59:29 +0800

    :sparkles: rewrite api model

---
+++ lib/constants/host.dart
@@ -0,0 +1,5 @@
+enum Host { dcard }
+
+Map<Host, String> hostMap = {
+  Host.dcard: 'dcard.tw',
+};
--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,13 +1,35 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:dcard_clone/constants/host.dart';
 
 class ApiModel {
+  Host host;
+  ApiModel([this.host = Host.dcard]);
+
   HttpClient client = new HttpClient();
 
-  Future<dynamic> fetch(String endpoint) async {
-    final request = await client.getUrl(Uri.parse(endpoint));
-    final response = await request.close();
-    final data = await utf8.decodeStream(response);
-    return json.decode(data);
+  Future<dynamic> fetch(String path, [Map<String, dynamic> query]) async {
+    final endpoint = Uri.https(
+        hostMap[this.host],
+        path,
+        query is Map
+            ? query.map((key, value) {
+                return MapEntry(key, value.toString());
+              })
+            : null);
+
+    try {
+      final request = await client.getUrl(endpoint);
+      final response = await request.close();
+      return response;
+    } catch (e) {
+      print('⚠️ [ApiModel]: $e');
+    }
   }
+}
+
+/// util for to json
+Future<dynamic> toJson(dynamic response) async {
+  final data = await utf8.decodeStream(response);
+  return json.decode(data);
 }
