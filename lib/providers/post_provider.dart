fba21c8bbb7759d32bc655c20e23054bbd422531
maxam2017 <whk0313@gmail.com>
2021-02-12 14:24:12 +0800

    :sparkles: implement selector

--- lib/providers/post_provider.dart
+++ lib/providers/post_provider.dart
@@ -1,15 +1,18 @@
-import 'package:dcard_clone/adapter/store.dart';
+import 'package:dcard_clone/adapter/pagination.dart';
 import 'package:dcard_clone/models/post.dart';
 import 'package:flutter/material.dart';
 
 FromJsonFunc<PostModel> fromJson = (json) => PostModel.fromJson(json);
 SelectIdFunc<PostModel> selectId = (post) => post.id.toString();
+GetPaginationKeyFunc getPaginationKey =
+    (query) => 'popular: ${query['popular']}';
 
-class PostProvider extends StoreAdapter<PostModel> with ChangeNotifier {
+class PostProvider extends PaginationAdapter<PostModel> with ChangeNotifier {
   PostProvider()
       : super(
           fromJson = fromJson,
           selectId = selectId,
+          getPaginationKey = getPaginationKey,
         );
 
   Future<void> listPost([Map<String, dynamic> query]) async {
